65b7423bbeb29e21e10fc3a2cd933c6954a1ee41
Amritesh Singh <amritesh6767@gmail.com>
2026-02-26 16:51:49 +0530

    commit

---
+++ backend/routes/assignment.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getAllAssignments,
+    getAssignmentById,
+    executeAssignmentQuery,
+    getAssignmentHint
+} = require('../controllers/assignmentController');
+
+// List assignments
+router.get('/', getAllAssignments);
+
+// Assignment details
+router.get('/:id', getAssignmentById);
+
+// Execution and feedback
+router.post('/:id/execute', executeAssignmentQuery);
+router.post('/:id/hint', getAssignmentHint);
+
+module.exports = router;
+
+
