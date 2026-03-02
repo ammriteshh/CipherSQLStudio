25495abf856fe3cdcc1c5469c17b7936257b8617
Amritesh Singh <amritesh6767@gmail.com>
2026-03-02 23:14:23 +0530

    commit

--- backend/routes/progress.routes.js
+++ backend/routes/progress.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getUserProgress, updateUserProgress } = require('../controllers/progressController');
 
-// Progress tracking
 router.get('/:userId', getUserProgress);
 router.post('/', updateUserProgress);
 
